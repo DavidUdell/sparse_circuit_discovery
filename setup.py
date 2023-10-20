affca5329086df85dd8eee4108d0bf766b22ed94
DavidUdell <udelldavidb@gmail.com>
2023-10-20 16:36:22 -0700

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""Install dependencies for `sparse_circuit_discovery`."""
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sparse_circuit_discovery",
+    description="Automatic circuit discovery in LLMs, using sparse coding.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="David Udell",
+    version="0.0.1",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "jupyter",
+        "numpy",
+        "torch",
+        "transformers",
+        "accelerate",
+        "lightning",
+        "scikit-learn",
+        "PyYAML",
+        "pytest",
+        "circuitsvis",
+        "prettytable",
+        "openai",
+    ],
+)
