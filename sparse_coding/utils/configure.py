9367b80316263f4a3a8379beb591c766b627cc56
DavidUdell <udelldavidb@gmail.com>
2023-11-06 23:39:50 +0000

    Now using absolute paths throughout, to enable clean script organization. This makes things slightly less portable, so maybe I'll have to return later on and write a smarter configuration function using smart relative paths.

--- sparse_coding/utils/configure.py
+++ sparse_coding/utils/configure.py
@@ -6,16 +6,28 @@ def load_yaml_constants():
     """Load config files with get() methods."""
 
     try:
-        with open("hf_access.yaml", "r", encoding="utf-8") as f:
+        with open(
+            "/root/sparse_circuit_discovery/sparse_coding/hf_access.yaml",
+            "r",
+            encoding="utf-8",
+        ) as f:
             access = yaml.safe_load(f)
     except FileNotFoundError:
         print("hf_access.yaml not found. Creating it now.")
-        with open("hf_access.yaml", "w", encoding="utf-8") as w:
+        with open(
+            "/root/sparse_circuit_discovery/sparse_coding/hf_access.yaml",
+            "w",
+            encoding="utf-8",
+        ) as w:
             w.write('HF_ACCESS_TOKEN: ""\n')
         access = {}
     except yaml.YAMLError as e:
         print(e)
-    with open("central_config.yaml", "r", encoding="utf-8") as f:
+    with open(
+        "/root/sparse_circuit_discovery/sparse_coding/central_config.yaml",
+        "r",
+        encoding="utf-8",
+    ) as f:
         try:
             config = yaml.safe_load(f)
         except yaml.YAMLError as e:
