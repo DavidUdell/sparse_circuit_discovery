c5bf35d8a5a6ed1a9e742fc1af7f9e98a8713cfa
DavidUdell <udelldavidb@gmail.com>
2024-02-08 00:10:21 +0000

    It wasn't quotation marks. I'm not sure what isn't being properly sanitized in the dot file.

--- sparse_coding/interp_tools/utils/graphs.py
+++ sparse_coding/interp_tools/utils/graphs.py
@@ -169,7 +169,9 @@ def label_highlighting(
             token = tokenizer.convert_ids_to_tokens(token)
             token = tokenizer.convert_tokens_to_string([token])
             token = html.escape(token)
-            token = token.replace('"', '\\"')
+            token = token.replace("##", " ")
+            token = token.replace("[", " ")
+            token = token.replace("]", " ")
             label += f'{cell_tag}{token}</td>'
         label += "</tr>"
 
