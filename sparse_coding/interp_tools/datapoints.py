a3a65fa3e6c39389b4107cd95495d4b04dcc50c4
DavidUdell <udelldavidb@gmail.com>
2024-02-24 04:59:52 +0000

    Printing the training datapoints too, to search with regex and explain any weird interp data I see.

--- sparse_coding/interp_tools/datapoints.py
+++ sparse_coding/interp_tools/datapoints.py
@@ -14,10 +14,13 @@ from sparse_coding.utils.interface import load_yaml_constants
 # %%
 # Load constants
 _, config = load_yaml_constants(__file__)
+NUM_SEQUENCES_EVALED = config.get("NUM_SEQUENCES_EVALED", 1000)
 NUM_SEQUENCES_INTERPED = config.get("NUM_SEQUENCES_INTERPED")
+SEED = config.get("SEED")
 
 # %%
-# Load the validation set
+# Load the dataset
+np.random.seed(SEED)
 dataset: list[list[str]] = load_dataset(
     "Elriggs/openwebtext-100k",
     split="train",
@@ -27,11 +30,20 @@ dataset_indices: np.ndarray = np.random.choice(
     size=len(dataset),
     replace=False,
 )
+
+# %%
+# Print the training datapoints
+train_indices: np.ndarray = dataset_indices[:NUM_SEQUENCES_EVALED]
+training_set: list[list[int]] = [dataset[i] for i in train_indices]
+
+for datapoint in training_set:
+    print(datapoint)
+
+# %%
+# Print the validation datapoints
 STARTING_META_IDX: int = len(dataset) - NUM_SEQUENCES_INTERPED
 eval_indices: np.ndarray = dataset_indices[STARTING_META_IDX:]
 eval_set: list[list[str]] = [dataset[i] for i in eval_indices]
 
-# %%
-# Print the validation datapoints
 for datapoint in eval_set:
     print(datapoint)
