139997b2457f3c5623b74e737ed1ef0b1bb1958a
DavidUdell <udelldavidb@gmail.com>
2024-06-11 03:53:49 +0000

    Finished first pass refactor for linear memory of cognition_graph.py

--- sparse_coding/interp_tools/cognition_graph.py
+++ sparse_coding/interp_tools/cognition_graph.py
@@ -17,12 +17,16 @@ from textwrap import dedent
 import numpy as np
 import torch as t
 from accelerate import Accelerator
+from pygraphviz import AGraph
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedModel
 from tqdm.auto import tqdm
 import wandb
 
 from sparse_coding.interp_tools.utils.computations import calc_act_diffs
-from sparse_coding.interp_tools.utils.graphs import graph_and_log
+from sparse_coding.interp_tools.utils.graphs import (
+    color_range_from_scalars,
+    label_highlighting,
+)
 from sparse_coding.interp_tools.utils.hooks import (
     hooks_manager,
     prepare_autoencoder_and_indices,
@@ -30,7 +34,10 @@ from sparse_coding.interp_tools.utils.hooks import (
 )
 from sparse_coding.utils.interface import (
     load_yaml_constants,
+    load_preexisting_graph,
     parse_slice,
+    sanitize_model_name,
+    save_paths,
     slice_to_range,
 )
 from sparse_coding.utils.tasks import recursive_defaultdict
@@ -134,6 +141,24 @@ layer_decoders, _ = prepare_autoencoder_and_indices(
     TOP_K_INFO_FILE,
     accelerator,
     __file__,
+)
+
+# Load preexisting graph, if applicable.
+graph = load_preexisting_graph(MODEL_DIR, GRAPH_DOT_FILE, __file__)
+if graph is None:
+    graph = AGraph(directed=True)
+
+minor_effects: int = 0
+total_effect: float = 0.0
+plotted_effect: float = 0.0
+
+save_graph_path: str = save_paths(
+    __file__,
+    f"{sanitize_model_name(MODEL_DIR)}/{GRAPH_FILE}",
+)
+save_dot_path: str = save_paths(
+    __file__,
+    f"{sanitize_model_name(MODEL_DIR)}/{GRAPH_DOT_FILE}",
 )
 
 # %%
@@ -274,7 +299,7 @@ for ablate_layer_idx in ablate_layer_range:
             )
             assert len(ordered_meta_indices) == len(cache_indices)
 
-            # Keep just the greatest effects, if applicable.
+            # Then keep just the greatest effects.
             MOST_AFFECTED_DIMENSIONS = []
             reference_dimensions = set(layer_dim_indices[ablate_layer_idx + 1])
 
@@ -296,30 +321,107 @@ for ablate_layer_idx in ablate_layer_range:
             base_case_activations,
         )
 
+        # Reduce effects to graph data.
+        for (_, __, downstream_index), effect in activation_diff.items():
+            effect: float = effect.item()
+            magnitude: float = abs(effect)
 
+            if magnitude <= THRESHOLD or effect == 0.0:
+                minor_effects += 1
+                total_effect += magnitude
+                continue
 
+            total_effect += magnitude
+            plotted_effect += magnitude
 
+            graph.add_node(
+                f"{ablate_layer_idx}.{dimension}",
+                label=label_highlighting(
+                    ablate_layer_idx,
+                    dimension,
+                    MODEL_DIR,
+                    TOP_K_INFO_FILE,
+                    LOGIT_TOKENS,
+                    tokenizer,
+                    f"{ablate_layer_idx}.{dimension}",
+                    log_probability_diff,
+                    __file__,
+                ),
+                shape="box",
+            )
 
+            graph.add_node(
+                f"{ablate_layer_idx + 1}.{downstream_index}",
+                label=label_highlighting(
+                    ablate_layer_idx + 1,
+                    downstream_index,
+                    MODEL_DIR,
+                    TOP_K_INFO_FILE,
+                    LOGIT_TOKENS,
+                    tokenizer,
+                    f"{ablate_layer_idx + 1}.{downstream_index}",
+                    log_probability_diff,
+                    __file__,
+                ),
+                shape="box",
+            )
 
+            color_min, color_max = color_range_from_scalars(activation_diff)
+            if effect > 0.0:
+                red: int = 0
+                blue: int = 255
+            elif effect < 0.0:
+                red: int = 255
+                blue: int = 0
+            alpha = int(
+                255 * magnitude / max(abs(color_max), abs(color_min))
+            )
+            rgba_str: str = f"#{red:02x}00{blue:02x}{alpha:02x}"
 
-# keepers: dict[tuple[int, int], int] = {}
+            graph.add_edge(
+                f"{ablate_layer_idx}.{dimension}",
+                f"{ablate_layer_idx + 1}.{downstream_index}",
+                color=rgba_str,
+            )
 
 # %%
-# Graph effects.
-graph_and_log(
-    act_diffs,
-    keepers,
-    BRANCHING_FACTOR,
-    MODEL_DIR,
-    GRAPH_FILE,
-    GRAPH_DOT_FILE,
-    TOP_K_INFO_FILE,
-    THRESHOLD,
-    LOGIT_TOKENS,
-    tokenizer,
-    probability_diffs,
-    __file__,
+# Cleanup graph.
+edges = graph.edges()
+assert len(edges) == len(set(edges)), "Repeat edges in graph."
+
+unlinked_nodes: int = 0
+for node in graph.nodes():
+    if len(graph.edges(node)) == 0:
+        graph.remove_node(node)
+        unlinked_nodes += 1
+
+fraction_included = round(plotted_effect / total_effect, 2)
+graph.add_node(
+    f"Effects plotted out of collected: ~{fraction_included*100}%."
 )
+
+print(
+    f"{minor_effects} minor effect(s) were ignored."
+    f" {unlinked_nodes} unlinked node(s) were dropped.\n"
+)
+
+# %%
+# Render and save graph.
+graph.write(save_dot_path)
+graph.draw(
+    save_graph_path,
+    format="svg",
+    prog="dot",
+)
+
+artifact = wandb.Artifact(
+    "cognition_graph",
+    type="directed_graph",
+)
+artifact.add_file(save_graph_path)
+wandb.log_artifact(artifact)
+
+print(f"Graph saved to {save_graph_path}.")
 
 # %%
 # Wrap up logging.
