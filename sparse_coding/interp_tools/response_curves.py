2b076c1c934528e65ed9a09001a9fcc6aeb6c5a2
DavidUdell <udelldavidb@gmail.com>
2024-06-27 23:22:03 +0000

    Progress on response curves script. I'm going to need to restore the COEFFICIENT kwarg in hooks_manager next.

--- sparse_coding/interp_tools/response_curves.py
+++ sparse_coding/interp_tools/response_curves.py
@@ -4,10 +4,16 @@
 
 import warnings
 import torch as t
+from accelerate import Accelerator
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     PreTrainedModel,
+)
+
+from sparse_coding.interp_tools.utils.hooks import (
+    hooks_manager,
+    prepare_autoencoder_and_indices,
 )
 
 from sparse_coding.utils.interface import (
@@ -25,11 +31,17 @@ ENCODER_FILE = config.get("ENCODER_FILE")
 ENC_BIASES_FILE = config.get("ENC_BIASES_FILE")
 DECODER_FILE = config.get("DECODER_FILE")
 DEC_BIASES_FILE = config.get("DEC_BIASES_FILE")
+TOP_K_INFO_FILE = config.get("TOP_K_INFO_FILE")
 SEED = config.get("SEED")
 # dict[int, list[int]]
 PINNED_ABLATION_DIM = {1: [1]}
 PINNED_CACHE_DIM = {2: [1]}
 COEFFICIENT: float = 0.0
+
+RANGE = range(
+    PINNED_ABLATION_DIM.keys()[0],
+    PINNED_CACHE_DIM.keys()[0],
+)
 
 # %%
 # Reproducibility.
@@ -45,4 +57,26 @@ with warnings.catch_warnings():
 tokenizer = AutoTokenizer.from_pretrained(
     MODEL_DIR
 )
+accelerator: Accelerator = Accelerator()
 
+# %%
+# Prepare relevant autoencoders.
+layer_encoders, layer_dim_indices = prepare_autoencoder_and_indices(
+    RANGE,
+    MODEL_DIR,
+    ENCODER_FILE,
+    ENC_BIASES_FILE,
+    TOP_K_INFO_FILE,
+    accelerator,
+    __file__,
+)
+
+layer_decoders, _ = prepare_autoencoder_and_indices(
+    RANGE,
+    MODEL_DIR,
+    DECODER_FILE,
+    DEC_BIASES_FILE,
+    TOP_K_INFO_FILE,
+    accelerator,
+    __file__,
+)
