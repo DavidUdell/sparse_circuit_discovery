b4f14320cf172102e22730765d2f7e67ec6e9bb4
DavidUdell <udelldavidb@gmail.com>
2024-07-24 03:54:18 +0000

    Progress writing the composite module I need for the Jacobian call. It still needs a constant bias module, for decoder_bias_2.

--- sparse_coding/interp_tools/utils/hooks.py
+++ sparse_coding/interp_tools/utils/hooks.py
@@ -330,6 +330,42 @@ def jacobians_manager(
     """
     jac_dict: dict = {}
 
+    def composite_module(current_module: t.nn.Module) -> t.nn.Sequential:
+        """The relevant torch modules, composed."""
+
+        decoder_1, dec_bias_1 = dec_tensors_per_layer[upstream_layer_idx]
+        encoder_2, enc_bias_2 = enc_tensors_per_layer[upstream_layer_idx + 1]
+
+        composed_mod = t.nn.Sequential(
+            t.nn.Linear(decoder_1.shape[1], decoder_1.shape[0]),
+            current_module,
+            # Constant bias.
+            t.nn.Linear(encoder_2.shape[1], encoder_2.shape[0]),
+            t.nn.ReLU(inplace=True),
+        )
+
+        composed_mod[0].weight, composed_mod[0].bias = decoder_1.T, dec_bias_1
+        composed_mod[2].weight, composed_mod[2].bias = encoder_2.T, enc_bias_2
+
+        #     t.nn.functional.linear(  # pylint: disable=not-callable
+        #         replaced_acts,
+        #         decoder.T.to(model.device),
+        #         bias=dec_biases.to(model.device),
+        #     ),
+        #     current_module,
+        #     t.nn.functional.linear(  # pylint: disable=not-callable
+        #         output[0] - dec_biases.to(model.device),
+        #         encoder.T.to(model.device),
+        #         bias=enc_biases.to(model.device),
+        #     ).to(model.device),
+        #     t.nn.functional.relu(
+        #         projected_acts,
+        #         inplace=True,
+        #     ),
+        # )
+
+        return composed_mod
+
     def splice_hook_fac(
         encoder: t.Tensor,
         enc_biases: t.Tensor,
