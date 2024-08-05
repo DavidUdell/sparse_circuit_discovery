a35a88145d097d48e598492bb428aa3e932b255c
DavidUdell <udelldavidb@gmail.com>
2024-08-05 13:06:04 +0000

    The usual graphing boilerplate.

---
+++ sparse_coding/interp_tools/grad_graph.py
@@ -0,0 +1,106 @@
+# %%
+"""
+Classic circuit discovery with gradient methods, with sparse autoencoders.
+
+I want to get a feel for this approach with a quick implementation here, in
+this interface and infrastructure.
+"""
+
+
+import torch as t
+from accelerate import Accelerator
+from pygraphviz import AGraph
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+)
+import wandb
+
+from sparse_coding.utils.interface import (
+    load_preexisting_graph,
+    load_yaml_constants,
+    parse_slice,
+    sanitize_model_name,
+    save_paths,
+    slice_to_range,
+)
+from sparse_coding.interp_tools.utils.hooks import (
+    prepare_autoencoder_and_indices,
+)
+
+# %%
+# Load constants.
+_, config = load_yaml_constants(__file__)
+
+WANDB_PROJECT = config.get("WANDB_PROJECT")
+WANDB_ENTITY = config.get("WANDB_ENTITY")
+MODEL_DIR = config.get("MODEL_DIR")
+PROMPT = config.get("PROMPT")
+ACTS_LAYERS_SLICE = parse_slice(config.get("ACTS_LAYERS_SLICE"))
+ENCODER_FILE = config.get("ENCODER_FILE")
+ENC_BIASES_FILE = config.get("ENC_BIASES_FILE")
+DECODER_FILE = config.get("DECODER_FILE")
+DEC_BIASES_FILE = config.get("DEC_BIASES_FILE")
+TOP_K_INFO_FILE = config.get("TOP_K_INFO_FILE")
+JACOBIANS_FILE = config.get("JACOBIANS_FILE")
+JACOBIANS_DOT_FILE = config.get("JACOBIANS_DOT_FILE")
+LOGIT_TOKENS = config.get("LOGIT_TOKENS", 10)
+SEED = config.get("SEED")
+
+NUM_TOP_EFFECTS: int = 10
+
+# %%
+# Reproducibility.
+_ = t.manual_seed(SEED)
+
+# %%
+# Log to wandb.
+wandb.init(
+    project=WANDB_PROJECT,
+    entity=WANDB_ENTITY,
+    config=config,
+)
+
+# %%
+# Load and prepare the model.
+model: AutoModelForCausalLM = AutoModelForCausalLM.from_pretrained(MODEL_DIR)
+tokenizer: AutoTokenizer = AutoTokenizer.from_pretrained(MODEL_DIR)
+accelerator: Accelerator = Accelerator()
+
+model = accelerator.prepare(model)
+layer_range = slice_to_range(model, ACTS_LAYERS_SLICE)
+up_layer_idx = layer_range[0]
+
+# %%
+# Prepare all layer range autoencoders.
+encoders_and_biases, _ = prepare_autoencoder_and_indices(
+    layer_range,
+    MODEL_DIR,
+    ENCODER_FILE,
+    ENC_BIASES_FILE,
+    TOP_K_INFO_FILE,
+    accelerator,
+    __file__,
+)
+decoders_and_biases, _ = prepare_autoencoder_and_indices(
+    layer_range,
+    MODEL_DIR,
+    DECODER_FILE,
+    DEC_BIASES_FILE,
+    TOP_K_INFO_FILE,
+    accelerator,
+    __file__,
+)
+
+# %%
+# Load preexisting graph, if available.
+graph = load_preexisting_graph(MODEL_DIR, JACOBIANS_DOT_FILE, __file__)
+if graph is None:
+    graph = AGraph(directed=True)
+
+save_graph_path: str = save_paths(
+    __file__, f"{sanitize_model_name(MODEL_DIR)}/{JACOBIANS_FILE}"
+)
+save_dot_path: str = save_paths(
+    __file__, f"{sanitize_model_name(MODEL_DIR)}/{JACOBIANS_DOT_FILE}"
+)
