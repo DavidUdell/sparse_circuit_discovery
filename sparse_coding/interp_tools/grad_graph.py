d836238481567bc9bc30d220fd6d19fb47ab56fc
DavidUdell <udelldavidb@gmail.com>
2024-10-08 22:37:32 +0000

    Commented dict keys for neuronpedia dict object.

--- sparse_coding/interp_tools/grad_graph.py
+++ sparse_coding/interp_tools/grad_graph.py
@@ -89,8 +89,45 @@ response = requests.get(
 )
 
 if response.status_code == 404:
-    raise ValueError("Neuronpedia API 404 error")
+    raise ValueError("404 error from Neuronpedia API")
 
+# modelId
+# layer
+# index
+# sourceSetName
+# creatorId
+# createdAt
+# maxActApprox
+# neuron_alignment_indices
+# neuron_alignment_values
+# neuron_alignment_l1
+# correlated_neurons_indices
+# correlated_neurons_pearson
+# correlated_neurons_l1
+# correlated_features_indices
+# correlated_features_pearson
+# correlated_features_l1
+# neg_str
+# neg_values
+# pos_str
+# pos_values
+# frac_nonzero
+# freq_hist_data_bar_heights
+# freq_hist_data_bar_values
+# logits_hist_data_bar_heights
+# logits_hist_data_bar_values
+# decoder_weights_dist
+# umap_cluster
+# umap_log_feature_sparsity
+# umap_x
+# umap_y
+# model
+# lists
+# source
+# sourceSet
+# activations
+# explanations
+# comments
 neuronpedia: dict = response.json()
 
 raise ValueError("REPL Breakpoint")
