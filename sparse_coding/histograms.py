a3dfdcfe12bdc102ac09aa0eddba0f25db57117b
DavidUdell <udelldavidb@gmail.com>
2024-10-29 23:46:59 +0000

    More building out histograms.py

--- sparse_coding/histograms.py
+++ sparse_coding/histograms.py
@@ -11,6 +11,7 @@ import torch as t
 from transformers import AutoModelForCausalLM, PreTrainedModel
 import wandb
 
+from sparse_coding.interp_tools.utils.computations import Encoder
 from sparse_coding.utils.interface import (
     load_yaml_constants,
     parse_slice,
@@ -23,10 +24,19 @@ from sparse_coding.utils.interface import (
 access, config = load_yaml_constants(__file__)
 
 HF_ACCESS_TOKEN = access.get("HF_ACCESS_TOKEN", "")
-MODEL_DIR = config.get("MODEL_DIR")
-ACTS_LAYERS_SLICE = parse_slice(config.get("ACTS_LAYERS_SLICE"))
 WANDB_PROJECT = config.get("WANDB_PROJECT")
 WANDB_ENTITY = config.get("WANDB_ENTITY")
+MODEL_DIR = config.get("MODEL_DIR")
+ACTS_LAYERS_SLICE = parse_slice(config.get("ACTS_LAYERS_SLICE"))
+RESID_ACTS_FILE = config.get("ACTS_DATA_FILE")
+ATTN_ACTS_FILE = config.get("ATTN_DATA_FILE")
+MLP_ACTS_FILE = config.get("MLP_DATA_FILE")
+RESID_ENCODER_FILE = config.get("ENCODER_FILE")
+RESID_BIASES_FILE = config.get("ENC_BIASES_FILE")
+ATTN_ENCODER_FILE = config.get("ATTN_ENCODER_FILE")
+ATTN_BIASES_FILE = config.get("ATTN_ENC_BIASES_FILE")
+MLP_ENCODER_FILE = config.get("MLP_ENCODER_FILE")
+MLP_BIASES_FILE = config.get("MLP_ENC_BIASES_FILE")
 SEED = config.get("SEED")
 
 # %%
@@ -42,6 +52,13 @@ wandb.init(
     config=config,
 )
 
+
+# %%
+# Compute histograms functionality
+def histograms():
+    """Compute histograms with `t.histc`."""
+
+
 # %%
 # Loop over all the model layers in the slice.
 with warnings.catch_warnings():
@@ -52,12 +69,21 @@ with warnings.catch_warnings():
     )
 seq_layer_indices: range = slice_to_range(model, ACTS_LAYERS_SLICE)
 
-
-# %%
-# Compute histograms functionality
-def histograms():
-    """Compute histograms with `t.histc`."""
-
+resid = {
+    "acts": RESID_ACTS_FILE,
+    "encoder": RESID_ENCODER_FILE,
+    "biases": RESID_BIASES_FILE,
+}
+attn = {
+    "acts": ATTN_ACTS_FILE,
+    "encoder": ATTN_ENCODER_FILE,
+    "biases": ATTN_BIASES_FILE,
+}
+mlp = {
+    "acts": MLP_ACTS_FILE,
+    "encoder": MLP_ENCODER_FILE,
+    "biases": MLP_BIASES_FILE,
+}
 
 # %%
 # Wrap up logging
