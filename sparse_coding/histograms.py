3159a657df01f9e1e52a3152272073fe4e08677f
DavidUdell <udelldavidb@gmail.com>
2024-10-30 00:17:01 +0000

    Import needed tensors for histograms

--- sparse_coding/histograms.py
+++ sparse_coding/histograms.py
@@ -8,13 +8,16 @@ import warnings
 
 import numpy as np
 import torch as t
-from transformers import AutoModelForCausalLM, PreTrainedModel
+from accelerate import Accelerator
+from transformers import AutoConfig, AutoModelForCausalLM, PreTrainedModel
 import wandb
 
 from sparse_coding.interp_tools.utils.computations import Encoder
 from sparse_coding.utils.interface import (
     load_yaml_constants,
     parse_slice,
+    sanitize_model_name,
+    save_paths,
     slice_to_range,
 )
 
@@ -37,6 +40,10 @@ ATTN_ENCODER_FILE = config.get("ATTN_ENCODER_FILE")
 ATTN_BIASES_FILE = config.get("ATTN_ENC_BIASES_FILE")
 MLP_ENCODER_FILE = config.get("MLP_ENCODER_FILE")
 MLP_BIASES_FILE = config.get("MLP_ENC_BIASES_FILE")
+tsfm_config = AutoConfig.from_pretrained(MODEL_DIR, token=HF_ACCESS_TOKEN)
+HIDDEN_DIM = tsfm_config.hidden_size
+PROJECTION_FACTOR = config.get("PROJECTION_FACTOR")
+PROJECTION_DIM = int(HIDDEN_DIM * PROJECTION_FACTOR)
 SEED = config.get("SEED")
 
 # %%
@@ -61,6 +68,7 @@ def histograms():
 
 # %%
 # Loop over all the model layers in the slice.
+accelerator: Accelerator = Accelerator()
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", FutureWarning)
     model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(
@@ -84,6 +92,42 @@ mlp = {
     "encoder": MLP_ENCODER_FILE,
     "biases": MLP_BIASES_FILE,
 }
+
+for layer_idx in seq_layer_indices:
+    for sublayer in [resid, attn, mlp]:
+        acts_file: str = sublayer["acts"]
+        encoder_file: str = sublayer["encoder"]
+        biases_file: str = sublayer["biases"]
+
+        encoder_path: str = save_paths(
+            __file__,
+            f"{sanitize_model_name(MODEL_DIR)}/{layer_idx}/{encoder_file}",
+        )
+        biases_path: str = save_paths(
+            __file__,
+            f"{sanitize_model_name(MODEL_DIR)}/{layer_idx}/{biases_file}",
+        )
+        acts_path: str = save_paths(
+            __file__,
+            f"{sanitize_model_name(MODEL_DIR)}/{layer_idx}/{acts_file}",
+        )
+        imported_weights: t.Tensor = t.load(encoder_path, weights_only=True).T
+        imported_biases: t.Tensor = t.load(biases_path, weights_only=True)
+        layer_acts_data: t.Tensor = accelerator.prepare(
+            t.load(acts_path, weights_only=True)
+        )
+
+        # Reassigning model variable to tell the garbage collector we're done
+        # with it now.
+        model: Encoder = accelerator.prepare(
+            Encoder(
+                imported_weights,
+                imported_biases,
+                HIDDEN_DIM,
+                PROJECTION_DIM,
+            )
+        )
+
 
 # %%
 # Wrap up logging
