87a268d5e780280b181b593b8164d88688b4838d
DavidUdell <udelldavidb@gmail.com>
2024-11-06 22:34:01 +0000

    Chore: fix line-too-long linter complaint

--- sparse_coding/interp_tools/cognition_graph_mc.py
+++ sparse_coding/interp_tools/cognition_graph_mc.py
@@ -2,8 +2,8 @@
 """
 Mess with autoencoder activation dims during `truthful_qa` and graph effects.
 
-`cognition_graph_mc` in particular tries a model agains the multiple-choice task
-on `truthful_qa`, where the model is teed up to answer a m/c question with
+`cognition_graph_mc` in particular tries a model agains the multiple-choice
+task on `truthful_qa`, where the model is teed up to answer a m/c question with
 widely believed but false choices. The base task is compared to the task in
 which autoencoder activations dimensions are surgically scaled during
 inference, at the crucial last sequence position, where the model is answering.
