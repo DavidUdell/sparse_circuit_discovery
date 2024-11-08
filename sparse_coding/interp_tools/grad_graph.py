3b0966af89c09bcb11e1e7128b513f4443da835b
DavidUdell <udelldavidb@gmail.com>
2024-11-08 01:09:49 +0000

    Chore: remove old commented-out code

--- sparse_coding/interp_tools/grad_graph.py
+++ sparse_coding/interp_tools/grad_graph.py
@@ -341,7 +341,7 @@ with grads_manager(
         else:
             weighted_prod = weighted_prod[:, -1, :].squeeze()
 
-        ### Thresholding down nodes -> indices
+        ### Thresholding down-nodes -> indices
         percentile: None | float = percentiles.get(loc, None)
         if percentile is None:
             if "error_" not in loc:
@@ -373,7 +373,7 @@ with grads_manager(
             if isinstance(indices, int):
                 indices: list = [indices]
             assert len(indices) > 0
-        ### End thresholding down nodes
+        ### End thresholding down-nodes
 
         for dim_idx in tqdm(indices, desc=loc):
             weighted_prod[dim_idx].backward(retain_graph=True)
@@ -459,33 +459,6 @@ for edges_str, down_nodes in marginal_grads_dict.items():
 
     up_layer_module: str = "".join(up_layer_split[:-1])
     down_layer_module: str = "".join(down_layer_split[:-1])
-
-    # Graph error statistics.
-    # if "error" in up_layer_module and "error" not in down_layer_module:
-    #     sublayer_unexplained: float = 0.0
-    #     for down_dim, up_values in down_nodes.items():
-    #         up_values = up_values.squeeze()
-    #         if up_values.dim() == 2:
-    #             up_values = up_values[-1, :]
-    #         assert up_values.dim() == 1
-
-    #         # Sublayer absolute effect unexplained.
-    #         sublayer_unexplained += abs(up_values).sum().item()
-
-    #     # Store sublayer unexplained effect.
-    #     if down_layer_str not in unexplained_dict:
-    #         unexplained_dict[down_layer_str] = sublayer_unexplained
-    #     else:
-    #         unexplained_dict[down_layer_str] += sublayer_unexplained
-
-    #     # Log overall unexplained effect.
-    #     effect_unexplained += sublayer_unexplained
-
-    #     continue
-
-    # All errors are skipped during graphing.
-    # if "error" in up_layer_module or "error" in down_layer_module:
-    #     continue
 
     sublayer_explained: float = 0.0
     for down_dim, up_values in tqdm(down_nodes.items(), desc=edges_str):
@@ -694,22 +667,6 @@ total_frac_explained = round(
     effect_explained / (effect_explained + effect_unexplained), 2
 )
 
-# for i in explained_dict:
-#     assert i in unexplained_dict
-# for i in unexplained_dict:
-#     assert i in explained_dict
-
-label: str = ""
-# for i, explained in explained_dict.items():
-#     if explained + unexplained_dict[i] == 0.0:
-#         sublayer_frac_explained: float = 0.0
-#         print(f"Sublayer {i} logged 0.0 effects in neurons and autoencoder.")
-#     else:
-#         sublayer_frac_explained = round(
-#             explained / (explained + unexplained_dict[i]), 2
-#         )
-#     label += f"\n{i}: ~{sublayer_frac_explained*100}%"
-
 # Nuke singleton nodes.
 for node in graph.nodes():
     if len(graph.edges(node)) == 0:
@@ -719,7 +676,6 @@ graph = prune_graph(graph)
 
 graph.add_node(
     f"Overall effect explained by autoencoders: ~{total_frac_explained*100}%"
-    + label
 )
 
 # %%
